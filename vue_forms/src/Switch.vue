370ae1c9b7f4a0b2154dcfa3a5cdf49711f82c45
Ravi Singh <ravionrails@gmail.com>
2019-07-20 10:36:37 +0530

    populating input field with v-model directive

---
+++ vue_forms/src/Switch.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div 
+      id="on"
+      @click="switched(true)"
+      :class= "{active: !value}">on</div>
+    <div 
+      id="Off"
+      @click="switched(false)"
+      :class= "{active: value}">Off</div>  
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['value'],
+    methods: {
+      switched(isOn) {
+        this.$emit('input', isOn);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #on, #Off{
+    width: 40px;
+    height: 20px;
+    background-color: lightgray;
+    padding: 2px;
+    display: inline-block;
+    margin: 10px -2px;
+    box-sizing: content-box;
+    cursor: pointer;
+    text-align: center;
+  }
+  #on:hover, #on.active{
+    background-color: lightgreen;
+  }
+  #Off:hover, #Off.active{
+    background-color: lightcoral;
+  }
+</style>
